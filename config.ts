40261065fee9d13ada6bfa87aeb9d686a3443d4d
Henry-Hiles <henry@henryhiles.com>
2024-03-04 12:13:17 -0500

    initial commit

---
+++ config.ts
@@ -0,0 +1,10 @@
+import Config from "@quados-tools/models/config.ts"
+import packages from "@quados-tools/flatpak-manager/packages.ts"
+
+export default Config.create({
+    installedPackages: [
+        packages.monophony,
+        packages.warehouse,
+        packages.heroicGamesLauncher,
+    ],
+})
