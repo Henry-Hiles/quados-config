87bf6c4b4df2f7d462f2325ee5b856550e3069bb
Henry-Hiles <henry@henryhiles.com>
2024-03-04 20:48:53 -0500

    Revamp config

--- config.ts
+++ config.ts
@@ -1,10 +1,6 @@
-import Config from "@quados/tools/models/config.ts"
-import packages from "@quados/tools/flatpak-manager/packages.ts"
+import Config from "@quados/tools/models/config"
+import packages from "./packages"
 
 export default Config.create({
-    installedPackages: [
-        packages.monophony,
-        packages.warehouse,
-        packages.heroicGamesLauncher,
-    ],
+    installedPackages: [packages.monophony],
 })
