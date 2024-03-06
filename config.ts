bef9e10165257d5f2b1af9f2e4bb0e23622e544f
Henry-Hiles <henry@henryhiles.com>
2024-03-06 13:31:11 -0500

    Config changes

--- config.ts
+++ config.ts
@@ -2,5 +2,5 @@ import Config from "@quados/tools/models/config"
 import packages from "./packages"
 
 export default Config.create({
-    installedPackages: [packages.warehouse],
+    pkgs: [packages.warehouse],
 })
