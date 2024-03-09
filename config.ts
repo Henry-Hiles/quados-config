3b1dbb9d1759ff87731e9e071c665d7c3d072cfa
Henry-Hiles <henry@henryhiles.com>
2024-03-08 21:12:25 -0500

    Rename

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import packages from "./packages"
 
 createConfig({
     pkgs: [
-        packages.warehouse.setCommit(
+        packages.warehouse.withCommit(
             "510449e16535f2b28ef0ae9a53b0df119494e32d36a5572e0db1c44e96d9b08f"
         ),
     ],
