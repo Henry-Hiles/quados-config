1aacc4f62cdd507b8c613f106007f6a8ccaf53ed
Henry-Hiles <henry@henryhiles.com>
2024-03-08 20:58:03 -0500

    Set commit to old commit

--- config.ts
+++ config.ts
@@ -2,5 +2,9 @@ import { createConfig } from "quados"
 import packages from "./packages"
 
 createConfig({
-    pkgs: [packages.warehouse],
+    pkgs: [
+        packages.warehouse.setCommit(
+            "510449e16535f2b28ef0ae9a53b0df119494e32d36a5572e0db1c44e96d9b08f"
+        ),
+    ],
 })
