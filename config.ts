4f5b954afad46008d99351be80fe696f621d9ffc
Henry-Hiles <henry@henryhiles.com>
2024-03-10 15:57:35 -0400

    Update stuff

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { createConfig } from "quados"
-import packages from "./packages"
+import pkgs from "./packages"
 
 createConfig({
     pkgs: [
-        packages.warehouse.withCommit(
+        pkgs.warehouse.withCommit(
             "510449e16535f2b28ef0ae9a53b0df119494e32d36a5572e0db1c44e96d9b08f"
         ),
     ],
