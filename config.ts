a537842c6a25bed91678e2db007016fd195c1c70
Henry-Hiles <henry@henryhiles.com>
2024-03-12 16:29:16 -0400

    Initial package config work

--- config.ts
+++ config.ts
@@ -6,11 +6,17 @@ export default createConfig({
         pkgs.warehouse.withCommit(
             "510449e16535f2b28ef0ae9a53b0df119494e32d36a5572e0db1c44e96d9b08f"
         ),
-        pkgs.vscodium.withConfig({
-            "file.json": {
-                some: "value",
-            }.toString(),
-            "otherFile.ini": "foo",
+        pkgs.VSCodium.withConfig({
+            files: {
+                "test.ini": {
+                    source: "foo",
+                },
+            },
+            symlinks: {
+                "test/test.json": {
+                    source: "config/test.json",
+                },
+            },
         }),
     ],
 })
