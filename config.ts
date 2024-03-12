1be0816bf33f3e5d704853759449c5361e46e362
Henry-Hiles <henry@henryhiles.com>
2024-03-12 14:22:10 -0400

    Fix weird casing

--- config.ts
+++ config.ts
@@ -6,5 +6,11 @@ export default createConfig({
         pkgs.warehouse.withCommit(
             "510449e16535f2b28ef0ae9a53b0df119494e32d36a5572e0db1c44e96d9b08f"
         ),
+        pkgs.vscodium.withConfig({
+            "file.json": {
+                some: "value",
+            }.toString(),
+            "otherFile.ini": "foo",
+        }),
     ],
 })
