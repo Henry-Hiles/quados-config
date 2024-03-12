529716f517ac9689fd8504da817a809dcd991cdf
Henry-Hiles <henry@henryhiles.com>
2024-03-11 20:53:04 -0400

    Add build sandboxing

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
-import { createConfig } from "quados"
 import pkgs from "./packages"
+import { createConfig } from "quados"
 
-createConfig({
+export default createConfig({
     pkgs: [
         pkgs.warehouse.withCommit(
             "510449e16535f2b28ef0ae9a53b0df119494e32d36a5572e0db1c44e96d9b08f"
